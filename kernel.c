cfb080370c4f43302026cbe39726c0287597229a
mlodywilkuuu <mlodywilkuu@gmail.com>
2026-06-06 09:14:40 +0200

    Create kernel.c

---
+++ kernel.c
@@ -0,0 +1,11 @@
+// kernel.c
+
+// Funkcja, która zostanie wywołana przez nasz kod asemblerowy
+void kernel_main() {
+    // 1. Inicjalizacja sterowników (UART, FrameBuffer, itp.)
+    
+    // 2. Pętla główna systemu (tu będziesz obsługiwać procesy)
+    while (1) {
+        // Tu będzie scheduler, który zarządza zadaniami
+    }
+}
